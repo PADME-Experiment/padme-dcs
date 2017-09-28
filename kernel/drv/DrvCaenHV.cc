c0fda45cff44860a5727535a3e58025f2bc7c480
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2017-09-28 19:26:02 +0200

    forgot to add drv

---
+++ kernel/drv/DrvCaenHV.cc
@@ -0,0 +1,177 @@
+#include "DrvCaenHV.h"   //in c file
+#include <caenlib/CAENHVWrapper.h>
+#include<iostream>
+
+  DrvCaenHV_except&
+DrvCaenHV_except::CAENWrapperRetStatus(int caenhandler,int retstatus, const std::string& add)
+{ std::string a(CAENHV_GetError(caenhandler));
+  std::string str("CAENHVWrapper returned error ("+std::to_string(retstatus)+") : '" + a + "'");
+  if(add.size()>0)str+="\n"+add;
+
+  switch (retstatus){
+    case CAENHV_SYSERR               : throw DrvCaenHV_except::Except_SYSERR               (str) ;break;
+    case CAENHV_WRITEERR             : throw DrvCaenHV_except::Except_WRITEERR             (str) ;break;
+    case CAENHV_READERR              : throw DrvCaenHV_except::Except_READERR              (str) ;break;
+    case CAENHV_TIMEERR              : throw DrvCaenHV_except::Except_TIMEERR              (str) ;break;
+    case CAENHV_DOWN                 : throw DrvCaenHV_except::Except_DOWN                 (str) ;break;
+    case CAENHV_NOTPRES              : throw DrvCaenHV_except::Except_NOTPRES              (str) ;break;
+    case CAENHV_SLOTNOTPRES          : throw DrvCaenHV_except::Except_SLOTNOTPRES          (str) ;break;
+    case CAENHV_NOSERIAL             : throw DrvCaenHV_except::Except_NOSERIAL             (str) ;break;
+    case CAENHV_MEMORYFAULT          : throw DrvCaenHV_except::Except_MEMORYFAULT          (str) ;break;
+    case CAENHV_OUTOFRANGE           : throw DrvCaenHV_except::Except_OUTOFRANGE           (str) ;break;
+    case CAENHV_EXECCOMNOTIMPL       : throw DrvCaenHV_except::Except_EXECCOMNOTIMPL       (str) ;break;
+    case CAENHV_GETPROPNOTIMPL       : throw DrvCaenHV_except::Except_GETPROPNOTIMPL       (str) ;break;
+    case CAENHV_SETPROPNOTIMPL       : throw DrvCaenHV_except::Except_SETPROPNOTIMPL       (str) ;break;
+    case CAENHV_PROPNOTFOUND         : throw DrvCaenHV_except::Except_PROPNOTFOUND         (str) ;break;
+    case CAENHV_EXECNOTFOUND         : throw DrvCaenHV_except::Except_EXECNOTFOUND         (str) ;break;
+    case CAENHV_NOTSYSPROP           : throw DrvCaenHV_except::Except_NOTSYSPROP           (str) ;break;
+    case CAENHV_NOTGETPROP           : throw DrvCaenHV_except::Except_NOTGETPROP           (str) ;break;
+    case CAENHV_NOTSETPROP           : throw DrvCaenHV_except::Except_NOTSETPROP           (str) ;break;
+    case CAENHV_NOTEXECOMM           : throw DrvCaenHV_except::Except_NOTEXECOMM           (str) ;break;
+    case CAENHV_SYSCONFCHANGE        : throw DrvCaenHV_except::Except_SYSCONFCHANGE        (str) ;break;
+    case CAENHV_PARAMPROPNOTFOUND    : throw DrvCaenHV_except::Except_PARAMPROPNOTFOUND    (str) ;break;
+    case CAENHV_PARAMNOTFOUND        : throw DrvCaenHV_except::Except_PARAMNOTFOUND        (str) ;break;
+    case CAENHV_NODATA               : throw DrvCaenHV_except::Except_NODATA               (str) ;break;
+    case CAENHV_DEVALREADYOPEN       : throw DrvCaenHV_except::Except_DEVALREADYOPEN       (str) ;break;
+    case CAENHV_TOOMANYDEVICEOPEN    : throw DrvCaenHV_except::Except_TOOMANYDEVICEOPEN    (str) ;break;
+    case CAENHV_INVALIDPARAMETER     : throw DrvCaenHV_except::Except_INVALIDPARAMETER     (str) ;break;
+    case CAENHV_FUNCTIONNOTAVAILABLE : throw DrvCaenHV_except::Except_FUNCTIONNOTAVAILABLE (str) ;break;
+    case CAENHV_SOCKETERROR          : throw DrvCaenHV_except::Except_SOCKETERROR          (str) ;break;
+    case CAENHV_COMMUNICATIONERROR   : throw DrvCaenHV_except::Except_COMMUNICATIONERROR   (str) ;break;
+    case CAENHV_NOTYETIMPLEMENTED    : throw DrvCaenHV_except::Except_NOTYETIMPLEMENTED    (str) ;break;
+    case CAENHV_CONNECTED            : throw DrvCaenHV_except::Except_CONNECTED            (str) ;break;
+    case CAENHV_NOTCONNECTED         : throw DrvCaenHV_except::Except_NOTCONNECTED         (str) ;break;
+    case CAENHV_OS                   : throw DrvCaenHV_except::Except_OS                   (str) ;break;
+    case CAENHV_LOGINFAILED          : throw DrvCaenHV_except::Except_LOGINFAILED          (str) ;break;
+    case CAENHV_LOGOUTFAILED         : throw DrvCaenHV_except::Except_LOGOUTFAILED         (str) ;break;
+    case CAENHV_LINKNOTSUPPORTED     : throw DrvCaenHV_except::Except_LINKNOTSUPPORTED     (str) ;break;
+    case CAENHV_USERPASSFAILED       : throw DrvCaenHV_except::Except_USERPASSFAILED       (str) ;break;
+  }
+}
+
+
+
+
+  void
+DrvCaenHV::ComInit( const std::string&ipaddr, const std::string&user, const std::string&pass)
+{
+  CAENHVRESULT ret;
+  CAENHV_SYSTEM_TYPE_t sys_type = (CAENHV_SYSTEM_TYPE_t) SY4527; // 0: SY1527, 2: SY4527
+  int link_type = LINKTYPE_TCPIP;
+  ret = CAENHV_InitSystem(sys_type, link_type, (void*)ipaddr.c_str(), user.c_str(), pass.c_str(), &fCaenCrateHandle);
+  if(ret != CAENHV_OK){
+    DrvCaenHV_except::CAENWrapperRetStatus(fCaenCrateHandle,ret,
+        "IP = "+ipaddr+"\nuser = "+user+"\npass = "+pass);
+  }
+}
+
+  void
+DrvCaenHV::ComDeinit()
+{
+  int ret = CAENHV_DeinitSystem(fCaenCrateHandle);
+  if(ret != CAENHV_OK)
+    DrvCaenHV_except::CAENWrapperRetStatus(fCaenCrateHandle,ret);
+}
+
+  void
+DrvCaenHV::GetCrateMap()
+{
+  ushort nrslots, *nrchlist, *sernumlist;
+  char *modellist,*descrlist;
+  unsigned char *firmwaremin,*firmawaremax;
+
+  int ret = CAENHV_GetCrateMap(fCaenCrateHandle,
+      &nrslots,            //ushort *NrOfSlot,
+      &nrchlist,           //ushort **NrofChList,
+      &modellist,          //char **ModelList,
+      &descrlist,          //char **DescriptionList,
+      &sernumlist,         //ushort **SerNumList,
+      &firmwaremin,        //uchar **FmwRelMinList,
+      &firmawaremax);      //uchar **FmwRelMaxList);
+
+  if(ret != CAENHV_OK)
+    DrvCaenHV_except::CAENWrapperRetStatus(fCaenCrateHandle,ret);
+  std::vector<std::string>modelVec;
+  std::vector<std::string>descrVec;
+  ConvCharListVector(nrslots,modellist,modelVec);
+  ConvCharListVector(nrslots,descrlist,descrVec);
+
+  for(int i=0;i<nrslots;++i){
+    std::cout
+      <<"nrchlist    ["<<i<<"]'"<<nrchlist         [i] <<"'"<<std::endl
+      <<"modellist   ["<<i<<"]'"<<modelVec        [i] <<"'"<<std::endl
+      <<"descrlist   ["<<i<<"]'"<<descrVec        [i] <<"'"<<std::endl
+      <<"sernumlist  ["<<i<<"]'"<<sernumlist       [i] <<"'"<<std::endl
+      <<"firmwaremin ["<<i<<"]'"<<int(firmwaremin  [i])<<"'"<<std::endl
+      <<"firmawaremax["<<i<<"]'"<<int(firmawaremax [i])<<"'"<<std::endl
+      <<std::endl;
+  }
+
+  std::cerr<<"bef del "<<__FILE__<<__LINE__<<__func__<<std::endl;
+  CAENHV_Free(nrchlist    );/// must be dealocated by the user
+  CAENHV_Free(modellist   );/// must be dealocated by the user
+  CAENHV_Free(descrlist   );/// must be dealocated by the user
+  CAENHV_Free(sernumlist  );/// must be dealocated by the user
+  CAENHV_Free(firmwaremin );/// must be dealocated by the user
+  CAENHV_Free(firmawaremax);/// must be dealocated by the user
+}
+
+
+  void
+DrvCaenHV::GetExecCommList() ///Get list of possible 
+{
+  ushort numcom;
+  char*comnamelist;
+
+  int ret=CAENHV_GetExecCommList(fCaenCrateHandle,&numcom,&comnamelist);
+  if(ret!=CAENHV_OK)
+    DrvCaenHV_except::CAENWrapperRetStatus(fCaenCrateHandle,ret);
+  std::vector<std::string> list;
+  ConvCharListVector(numcom,comnamelist,list);
+  int len=0;
+  for(int i=0;i<numcom;++i){
+    std::cout<<list[i]<<"   ";
+  } std::cout<<std::endl;
+
+  CAENHV_Free(comnamelist);
+
+}
+
+
+  void
+DrvCaenHV::GetSysPropList() ///Get list of possible 
+{
+  ushort numcom;
+  char*comnamelist;
+
+  int ret=CAENHV_GetSysPropList(fCaenCrateHandle,&numcom,&comnamelist);
+  if(ret!=CAENHV_OK)
+    DrvCaenHV_except::CAENWrapperRetStatus(fCaenCrateHandle,ret);
+
+  std::vector<std::string> list;
+  ConvCharListVector(numcom,comnamelist,list);
+
+  int len=0;
+  for(int i=0;i<numcom;++i){
+    std::cout<<list[i]<<"   ";
+  } std::cout<<std::endl;
+
+  CAENHV_Free(comnamelist);
+
+}
+
+
+
+
+
+  void
+DrvCaenHV::ConvCharListVector(unsigned short n,const char* l,std::vector<std::string>&v)
+{
+  v.clear();
+  int len=0;
+  for(int i=0;i<n;++i){
+    std::string tmp(&l[len]);
+    len+=tmp.size()+1;
+    v.push_back(tmp);
+  }
+}
---
+++ kernel/drv/DrvCaenHV.h
@@ -0,0 +1,133 @@
+#ifndef  _DCS_kernel_drv_DrvCaenHV_h_
+#define  _DCS_kernel_drv_DrvCaenHV_h_ 1
+#include "DrvCaenHV.h"   //in c file
+
+#include<vector>
+
+
+#include "fwk/fwkException.hh"
+class DrvCaenHV_except:public fwk::Exception{
+  public:
+    class Except_OK                   ;
+    class Except_SYSERR               ;
+    class Except_WRITEERR             ;
+    class Except_READERR              ;
+    class Except_TIMEERR              ;
+    class Except_DOWN                 ;
+    class Except_NOTPRES              ;
+    class Except_SLOTNOTPRES          ;
+    class Except_NOSERIAL             ;
+    class Except_MEMORYFAULT          ;
+    class Except_OUTOFRANGE           ;
+    class Except_EXECCOMNOTIMPL       ;
+    class Except_GETPROPNOTIMPL       ;
+    class Except_SETPROPNOTIMPL       ;
+    class Except_PROPNOTFOUND         ;
+    class Except_EXECNOTFOUND         ;
+    class Except_NOTSYSPROP           ;
+    class Except_NOTGETPROP           ;
+    class Except_NOTSETPROP           ;
+    class Except_NOTEXECOMM           ;
+    class Except_SYSCONFCHANGE        ;
+    class Except_PARAMPROPNOTFOUND    ;
+    class Except_PARAMNOTFOUND        ;
+    class Except_NODATA               ;
+    class Except_DEVALREADYOPEN       ;
+    class Except_TOOMANYDEVICEOPEN    ;
+    class Except_INVALIDPARAMETER     ;
+    class Except_FUNCTIONNOTAVAILABLE ;
+    class Except_SOCKETERROR          ;
+    class Except_COMMUNICATIONERROR   ;
+    class Except_NOTYETIMPLEMENTED    ;
+    class Except_CONNECTED            ;
+    class Except_NOTCONNECTED         ;
+    class Except_OS                   ;
+    class Except_LOGINFAILED          ;
+    class Except_LOGOUTFAILED         ;
+    class Except_LINKNOTSUPPORTED     ;
+    class Except_USERPASSFAILED       ;
+  public:
+
+    //DrvCaenHV_except(){fType="DrvCaenHV_except";}
+    static DrvCaenHV_except& CAENWrapperRetStatus(int i,int retstat,const std::string& add=std::string());
+    DrvCaenHV_except(const std::string&a):fwk::Exception(a){}
+    //virtual ~DrvCaenHV_except(){}
+};
+
+
+
+class DrvCaenHV_except::Except_OK                   : public DrvCaenHV_except {public: Except_OK                  (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_SYSERR               : public DrvCaenHV_except {public: Except_SYSERR              (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_WRITEERR             : public DrvCaenHV_except {public: Except_WRITEERR            (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_READERR              : public DrvCaenHV_except {public: Except_READERR             (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_TIMEERR              : public DrvCaenHV_except {public: Except_TIMEERR             (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_DOWN                 : public DrvCaenHV_except {public: Except_DOWN                (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTPRES              : public DrvCaenHV_except {public: Except_NOTPRES             (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_SLOTNOTPRES          : public DrvCaenHV_except {public: Except_SLOTNOTPRES         (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOSERIAL             : public DrvCaenHV_except {public: Except_NOSERIAL            (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_MEMORYFAULT          : public DrvCaenHV_except {public: Except_MEMORYFAULT         (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_OUTOFRANGE           : public DrvCaenHV_except {public: Except_OUTOFRANGE          (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_EXECCOMNOTIMPL       : public DrvCaenHV_except {public: Except_EXECCOMNOTIMPL      (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_GETPROPNOTIMPL       : public DrvCaenHV_except {public: Except_GETPROPNOTIMPL      (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_SETPROPNOTIMPL       : public DrvCaenHV_except {public: Except_SETPROPNOTIMPL      (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_PROPNOTFOUND         : public DrvCaenHV_except {public: Except_PROPNOTFOUND        (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_EXECNOTFOUND         : public DrvCaenHV_except {public: Except_EXECNOTFOUND        (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTSYSPROP           : public DrvCaenHV_except {public: Except_NOTSYSPROP          (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTGETPROP           : public DrvCaenHV_except {public: Except_NOTGETPROP          (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTSETPROP           : public DrvCaenHV_except {public: Except_NOTSETPROP          (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTEXECOMM           : public DrvCaenHV_except {public: Except_NOTEXECOMM          (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_SYSCONFCHANGE        : public DrvCaenHV_except {public: Except_SYSCONFCHANGE       (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_PARAMPROPNOTFOUND    : public DrvCaenHV_except {public: Except_PARAMPROPNOTFOUND   (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_PARAMNOTFOUND        : public DrvCaenHV_except {public: Except_PARAMNOTFOUND       (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NODATA               : public DrvCaenHV_except {public: Except_NODATA              (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_DEVALREADYOPEN       : public DrvCaenHV_except {public: Except_DEVALREADYOPEN      (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_TOOMANYDEVICEOPEN    : public DrvCaenHV_except {public: Except_TOOMANYDEVICEOPEN   (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_INVALIDPARAMETER     : public DrvCaenHV_except {public: Except_INVALIDPARAMETER    (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_FUNCTIONNOTAVAILABLE : public DrvCaenHV_except {public: Except_FUNCTIONNOTAVAILABLE(const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_SOCKETERROR          : public DrvCaenHV_except {public: Except_SOCKETERROR         (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_COMMUNICATIONERROR   : public DrvCaenHV_except {public: Except_COMMUNICATIONERROR  (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTYETIMPLEMENTED    : public DrvCaenHV_except {public: Except_NOTYETIMPLEMENTED   (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_CONNECTED            : public DrvCaenHV_except {public: Except_CONNECTED           (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_NOTCONNECTED         : public DrvCaenHV_except {public: Except_NOTCONNECTED        (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_OS                   : public DrvCaenHV_except {public: Except_OS                  (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_LOGINFAILED          : public DrvCaenHV_except {public: Except_LOGINFAILED         (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_LOGOUTFAILED         : public DrvCaenHV_except {public: Except_LOGOUTFAILED        (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_LINKNOTSUPPORTED     : public DrvCaenHV_except {public: Except_LINKNOTSUPPORTED    (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+class DrvCaenHV_except::Except_USERPASSFAILED       : public DrvCaenHV_except {public: Except_USERPASSFAILED      (const std::string&s):DrvCaenHV_except(s){ fwk::Exception::SetType(*this); }; };
+
+class DrvCaenHV{
+  public:
+    DrvCaenHV(){
+      throw  DrvCaenHV_except::Except_OK
+        ("whatstr") ;
+      ComInit();
+      GetCrateMap();
+      GetExecCommList();
+      GetSysPropList();
+    }
+    ~DrvCaenHV(){
+      ComDeinit();
+    }
+  private:
+    void ComDeinit();
+    void ComInit(
+        const std::string&ipaddr="192.168.0.1",
+        const std::string&user="admin",
+        const std::string&pass="admin");
+    void GetCrateMap();
+    void GetExecCommList();
+    void GetSysPropList();
+
+
+
+  private:
+    int fCaenCrateHandle;
+
+
+  private:
+    void ConvCharListVector(unsigned short n,const char*
+        l,std::vector<std::string>&v);
+};
+
+
+#endif
